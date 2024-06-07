b383749da8531384aff8b2554acfa1f8af1e5f7a
Kaley Kwan <kaleymkwan@gmail.com>
2024-06-07 16:23:28 -0700

    add stylesheet

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,36 +1,40 @@
 import { Link } from "expo-router";
-import { SafeAreaView, Text, View, Pressable } from "react-native";
+import { SafeAreaView, Text, View, Pressable, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <Text
-        style={{
-          marginLeft: "10%",
-          marginTop: "2%",
-          fontSize: 25,
-          fontWeight: 700,
-        }}
-      >
-        Explore
-      </Text>
-      <View
-        style={{
-          flex: 1,
-          alignItems: "center",
-          margin: "10%",
-          marginTop: "7%",
-          backgroundColor: "white",
-          borderRadius: 15
-        }}
-      >
+      <Text style={styles.title}>Explore</Text>
+      <View style={styles.gameCard}>
         <Text>Pinpoint</Text>
         <Link href="/profile" asChild>
-          <Pressable style={{backgroundColor: "black", padding: "4%", borderRadius: 10}}>
-            <Text style={{color: "white"}}>Play</Text>
+          <Pressable style={styles.playButton}>
+            <Text style={{ color: "white" }}>Play</Text>
           </Pressable>
         </Link>
       </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  gameCard: {
+    flex: 1,
+    alignItems: "center",
+    margin: "10%",
+    marginTop: "7%",
+    backgroundColor: "white",
+    borderRadius: 15,
+  },
+  playButton: {
+    backgroundColor: "black",
+    padding: "4%",
+    borderRadius: 10,
+  },
+  title: {
+    marginLeft: "10%",
+    marginTop: "2%",
+    fontSize: 25,
+    fontWeight: "700",
+  },
+});
