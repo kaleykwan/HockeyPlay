2ca193daa3404ffb95521bd0a7c4061b4b113034
Kaley Kwan <kaleymkwan@gmail.com>
2024-06-18 17:31:56 -0700

    add useReducer

--- components/Pinpoint/PinpointGame.tsx
+++ components/Pinpoint/PinpointGame.tsx
@@ -1,6 +1,6 @@
 import PinpointDropdown from "@/components/Pinpoint/PinpointDropdown";
 import { Link } from "expo-router";
-import { useCallback, useState } from "react";
+import { useCallback, useReducer, useState } from "react";
 import {
   View,
   StyleSheet,
@@ -11,6 +11,56 @@ import {
 } from "react-native";
 import Animated, { FadeIn, FadeOut } from "react-native-reanimated";
 
+interface reducerState {
+  revealed: number;
+  guesses: string[];
+  gameOver: boolean;
+  solved: boolean;
+}
+
+interface reducerActionState {
+  type: string;
+  team?: string;
+}
+
+function createInitialState() {
+  return {
+    revealed: 1,
+    guesses: [],
+    gameOver: false,
+    solved: false,
+  };
+}
+
+function reducer(state: reducerState, action) {
+  switch (action.type) {
+    case "increment_revealed": {
+      return {
+        ...state,
+        revealed: state.revealed + 1,
+        guesses: [...state.guesses, action.team],
+      };
+    }
+    case "game_over_solved": {
+      return {
+        ...state,
+        revealed: 5,
+        gameOver: true,
+        solved: true,
+      };
+    }
+    case "game_over_unsolved": {
+      return {
+        ...state,
+        revealed: 5,
+        gameOver: true,
+        guesses: [...state.guesses, action.team],
+      };
+    }
+  }
+  throw Error("Unknown action: " + action.type);
+}
+
 export default function PinpointGame() {
   const [clues, setClues] = useState<string[]>([
     "Michael Bunting",
@@ -20,46 +70,35 @@ export default function PinpointGame() {
     "Auston Matthews",
   ]);
   const [answer, setAnswer] = useState<string>("Toronto Maple Leafs");
-  const [revealed, setRevealed] = useState<number>(1);
-  const [guesses, setGuesses] = useState<string[]>([]);
-  const [gameOver, setGameOver] = useState<boolean>(false);
-  const [solved, setSolved] = useState<boolean>(false);
 
-  // maybe useReducer
-  const [gameStatus, setGameStatus] = useState<{
-    answer: string;
-    guesses: string[];
-    gameOver: boolean;
-    solved: boolean;
-  }>({
-    answer: "Toronto Maple Leafs",
-    guesses: [],
-    gameOver: false,
-    solved: false,
-  });
+  const [state, dispatch] = useReducer(reducer, null, createInitialState);
 
   const incrementRevealed = useCallback(
     (team: string) => {
       if (team == answer) {
-        setGameOver(true);
-        setSolved(true);
-        setRevealed(5);
+        dispatch({
+          type: "game_over_solved",
+        });
+      } else if (state.revealed + 1 == 5) {
+        dispatch({
+          type: "game_over_unsolved",
+          team: team,
+        });
       } else {
-        setRevealed((prev) => prev + 1);
-        setGuesses((prev) => [...prev, team]);
-        if (revealed == 4) {
-          setGameOver(true);
-        }
+        dispatch({
+          type: "increment_revealed",
+          team: team,
+        });
       }
     },
-    [answer, revealed]
+    [answer, state.revealed]
   );
 
   const renderClues = useCallback(
     (clue: string, index: number) => {
       return (
         <View key={`${clue}-${index}`} style={pinpointStyles.revealedClue}>
-          {index < revealed && (
+          {index < state.revealed && (
             <Animated.Text
               entering={FadeIn.duration(550)}
               exiting={FadeOut}
@@ -71,7 +110,7 @@ export default function PinpointGame() {
         </View>
       );
     },
-    [revealed]
+    [state.revealed]
   );
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
@@ -79,9 +118,9 @@ export default function PinpointGame() {
         <View style={pinpointStyles.clueContainer}>
           {clues.map(renderClues)}
         </View>
-        {guesses.length > 0 && (
+        {state.guesses.length > 0 && (
           <View style={pinpointStyles.guessContainer}>
-            {guesses.map((team: string, index: number) => (
+            {state.guesses.map((team: string, index: number) => (
               <Text style={pinpointStyles.guess} key={index}>
                 {team}
               </Text>
@@ -89,17 +128,17 @@ export default function PinpointGame() {
           </View>
         )}
         <View>
-          {!gameOver && (
+          {!state.gameOver && (
             <PinpointDropdown incrementRevealed={incrementRevealed} />
           )}
         </View>
         <View>
-          {gameOver && (
+          {state.gameOver && (
             <View>
-              {solved && (
+              {state.solved && (
                 <Text style={pinpointStyles.solved}>You solved it!</Text>
               )}
-              {!solved && (
+              {!state.solved && (
                 <Text style={pinpointStyles.solved}>Better luck tomorrow!</Text>
               )}
               <Text style={pinpointStyles.answerText}>Answer: {answer}</Text>
